8d6ef8f276997068167043244a20db589d443cbb
unknown <steponas@hotmail.com>
2018-11-15 23:57:43 +0200

    working webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'src/index.js'),
+	output: {
+		path: path.join(__dirname, 'build'),
+		publicPath: '/',
+		filename: 'bundle.js',
+		chunkFilename: '[name].chunk.js',
+	},
+	devtool: 'cheap-module-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				options: {
+					// This is a feature of `babel-loader` for webpack (not Babel itself).
+					// It enables caching results in ./node_modules/.cache/babel-loader/
+					// directory for faster rebuilds.
+					cacheDirectory: true,
+					plugins: ['react-hot-loader/babel'],
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+							modules: true,
+							localIdentName: '[name]_[local]_[hash:base54:5]',
+						},
+					},
+					'sass-loader',
+				],
+			},
+		],
+	},
+	devServer: {
+		port: 3500,
+		publicPath: '/',
+		historyApiFallback: true,
+		hotOnly: true,
+		// hot: true,
+		contentBase: path.resolve(__dirname, 'public'),
+		watchContentBase: true,
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+			filename: path.resolve(__dirname, 'build/index.html'),
+			title: 'Mediapark practical assignment',
+			minify: true,
+			inject: 'body',
+		}),
+	],
+};
